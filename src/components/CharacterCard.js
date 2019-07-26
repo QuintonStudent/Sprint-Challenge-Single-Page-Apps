2cfe7ce09b02859a2b209da8b4c4535b31d4b8e0
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-07-26 12:12:26 -0400

    created axios request and pushed props to CharacterCard component. going to style with styled components now

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard ({ name, status, origin, image, species, location, episodes }) {
+  return (
+    <div className='card'>
+      <img src={image} />
+      <div>{name}</div>
+      <div>{species} {status}</div>
+      <br />
+      <div>Location: {location}</div>
+      <div>Origin: {origin}</div>
+      <br />
+      <button>episodes</button>
+    </div>
+  )
 }
